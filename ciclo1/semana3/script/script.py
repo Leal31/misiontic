c9a0b9a7eaefd84fc89971c67191941bd022cf96
Leal31 <lealemanuel31@protonmail.com>
2021-05-19 12:42:25 -0500

    semana4 clase1

--- ciclo1/semana3/script/script.py
+++
@@ -1,9 +0,0 @@
-from os import system
-
-def main():
-    system('cd C:/Users/leale/OneDrive/Documentos/proyectos/misiontic/ciclo1/semana3/reto2/')
-    system('dir')
-    system('cat script.py')
-
-if __name__ == '__main__':
-    main()
